832fa9ff9e74bd123862c351f6599b71d21514e8
Nathan Henrie <nate@n8henrie.com>
2024-07-10 13:52:13 -0600

    Fix for macOS

--- default.nix
+++ default.nix
@@ -5,16 +5,19 @@
 }:
 let
   inherit (rustPlatform) buildRustPackage cargoInstallHook;
-in
-buildRustPackage {
-  pname = "nix-rust-debug-info";
   buildType = "debug";
-  dontStrip = true;
-  version = "0.0.1";
-  src = ./.;
-  cargoHash = "sha256-3FzjK5hbqA3PNgzhanCvisynkUKK4/9BCDbPFu5FUxA=";
-}
-// lib.mkIf (buildRustPackage.buildType == "debug" && stdenv.isDarwin) {
-  RUSTFLAGS = "-C split-debuginfo=packed";
-  postInstall = "cp -RL ./target/${cargoInstallHook.targetSubdirectory}/debug/*.dSYM $out/bin/";
-}
+in
+buildRustPackage (
+  {
+    inherit ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package) name;
+    inherit buildType;
+    dontStrip = true;
+    version = "0.0.1";
+    src = ./.;
+    cargoHash = "sha256-liwY1EAeCA+0y+GegJdqV3PKzVHN9noD6L5lplqgrus=";
+  }
+  // lib.optionalAttrs (buildType == "debug" && stdenv.isDarwin) {
+    RUSTFLAGS = "-C split-debuginfo=packed";
+    postInstall = "cp -RL ./target/${cargoInstallHook.targetSubdirectory}/debug/*.dSYM $out/bin/";
+  }
+)
--- flake.nix
+++ flake.nix
@@ -1,9 +1,20 @@
 {
+  description = "https://github.com/NixOS/nixpkgs/issues/282900 https://github.com/NixOS/nixpkgs/issues/262131";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   outputs =
     { nixpkgs, ... }:
-    {
-      packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.callPackage ./. { };
-      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./. { };
-    };
+    let
+      systems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      eachSystem =
+        with nixpkgs.lib;
+        f: foldAttrs mergeAttrs { } (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems);
+    in
+    eachSystem (system: {
+      packages.default = nixpkgs.legacyPackages.${system}.callPackage ./. { };
+    });
 }
